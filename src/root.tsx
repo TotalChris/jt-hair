18aa2b9138d0f8bb6c3b9c1ff6794f5601e4e8c0
Chris Yates <chris@chrisyates.dev>
2025-06-15 21:31:06 -0400

    - Import icons - Link manifest - Fix linter catching router-generated files

--- src/root.tsx
+++ src/root.tsx
@@ -6,6 +6,12 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/assets/icons/application/jt.svg"
+        />
+        <link rel="manifest" href="manifest.json" />
         <title>JT Hair Care of Brighton</title>
       </head>
       <body>
